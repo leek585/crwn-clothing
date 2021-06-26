69f4b75ff1243f412861e2b06705222dc180b612
Ka Hui <kahui114@gmail.com>
2021-06-26 12:17:42 +0800

    added Stripe integration and changed favicon icon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IT2n5DIEZMdSRpIk38YgP2LcvdWXj0MendayK6hgW6KRHlu1LgAA0TtwGV7mQ1s2KWRc0mc5GVuaxNOZI33x0xr00UrVILWwS";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/en/f3eb2117da"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
